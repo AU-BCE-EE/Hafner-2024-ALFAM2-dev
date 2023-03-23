955ae76eb5a97f0719f05f9ec6b7fc0261f11e6e
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-03-23 06:44:57 -0400

    Tweak

--- r5/scripts/plot.R
+++ r5/scripts/plot.R
@@ -1,10 +1,8 @@
 vl <- data.table(ct = c(12, 100), rain = 'Rain')
 
-ggplot(preds, aes(ct, er, colour = pars)) +
-  geom_vline(data = vl, aes(xintercept = ct), lty = '1111') +
+ggplot(preds, aes(ct, er, colour = pars, lty = rain, group = sim)) +
   geom_line() +
-  facet_wrap(~ rain) +
   theme_bw() +
-  theme(legend.position = 'top') +
-  labs(x = 'Time since application (h)', y = 'Relative emis. (frac. TAN)', colour = 'Parameter set')
-ggsave2x('../plots/r5_demo', height = 3, width = 4)
+  theme(legend.position = 'right') +
+  labs(x = 'Time since application (h)', y = 'Relative emis. (frac. TAN)', colour = 'Parameter set', lty = 'Rain')
+ggsave2x('../plots/r5_demo', height = 2.3, width = 3.5)
--- r5/scripts/run.R
+++ r5/scripts/run.R
@@ -6,9 +6,9 @@ pred2 <- data.table(alfam2(dat.rain, app.name = 'TAN.app', time.name = 'ct'))
 pred3 <- data.table(alfam2(dat.ref, pars = pars03, app.name = 'TAN.app', time.name = 'ct'))
 pred4 <- data.table(alfam2(dat.rain, pars = pars03, app.name = 'TAN.app', time.name = 'ct'))
 
-pred1[, `:=` (sim = 'A', rain = 'No rain', pars = 'Par. set 2')]
-pred2[, `:=` (sim = 'B', rain = 'Rain', pars = 'Par. set 2')]
-pred3[, `:=` (sim = 'C', rain = 'No rain', pars = 'Par. set 3')]
-pred4[, `:=` (sim = 'D', rain = 'Rain', pars = 'Par. set 3')]
+pred1[, `:=` (sim = 'A', rain = 'No rain', pars = '2')]
+pred2[, `:=` (sim = 'B', rain = 'Rain', pars = '2')]
+pred3[, `:=` (sim = 'C', rain = 'No rain', pars = '3')]
+pred4[, `:=` (sim = 'D', rain = 'Rain', pars = '3')]
 
 preds <- rbind(pred1, pred2, pred3, pred4)
