b4ae59232b50630fd6a9b406cb442c63464fbdbe
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-03-23 14:21:47 -0400

    Add flux plot, tweaks

--- r5/scripts/load_pars.R
+++ r5/scripts/load_pars.R
@@ -5,4 +5,4 @@ names(pars03) <- colnames(pars)
 pars03 <- pars03[!is.na(pars03)]
 
 pars025 <- ALFAM2pars02
-pars025 <- c(pars025, int.r5 = -2.2)
+pars025 <- c(pars025, int.r5 = -2.1)
--- r5/scripts/plot.R
+++ r5/scripts/plot.R
@@ -5,4 +5,11 @@ ggplot(preds, aes(ct, er, colour = pars, lty = rain, group = sim)) +
   theme_bw() +
   theme(legend.position = 'right') +
   labs(x = 'Time since application (h)', y = 'Relative emis. (frac. TAN)', colour = 'Parameter set', lty = '')
-ggsave2x('../plots/r5_demo', height = 2.3, width = 3.5)
+ggsave2x('../plots/r5_emis_demo', height = 2.3, width = 3.5)
+
+ggplot(preds, aes(ct, log10(j), colour = pars, lty = rain, group = sim)) +
+  geom_line() +
+  theme_bw() +
+  theme(legend.position = 'right') +
+  labs(x = 'Time since application (h)', y = expression('Flux'~(kg~h^'-1'~ha^'-1')), colour = 'Parameter set', lty = '')
+ggsave2x('../plots/r5_flux_demo', height = 2.3, width = 3.5)
