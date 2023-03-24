0b32fd6770c3a37976fa30f46dd8f10ecb78018c
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-03-24 14:48:50 -0400

    add plots

--- r5/scripts/plot.R
+++ r5/scripts/plot.R
@@ -5,11 +5,18 @@ ggplot(preds, aes(ct, er, colour = pars, lty = rain, group = sim)) +
   theme_bw() +
   theme(legend.position = 'right') +
   labs(x = 'Time since application (h)', y = 'Relative emis. (frac. TAN)', colour = 'Parameter set', lty = '')
-ggsave2x('../plots/r5_emis_demo', height = 2.3, width = 3.5)
+ggsave2x('../plots/r5_demo_emis', height = 2.3, width = 3.5)
 
 ggplot(preds, aes(ct, log10(j), colour = pars, lty = rain, group = sim)) +
   geom_line() +
   theme_bw() +
   theme(legend.position = 'right') +
-  labs(x = 'Time since application (h)', y = expression('Flux'~(kg~h^'-1'~ha^'-1')), colour = 'Parameter set', lty = '')
-ggsave2x('../plots/r5_flux_demo', height = 2.3, width = 3.5)
+  labs(x = 'Time since application (h)', y = expression(Log[10]~'flux'~(kg~h^'-1'~ha^'-1')), colour = 'Parameter set', lty = '')
+ggsave2x('../plots/r5_demo_flux', height = 2.3, width = 3.5)
+
+ggplot(preds, aes(ct, s, colour = pars, lty = rain, group = sim)) +
+  geom_line() +
+  theme_bw() +
+  theme(legend.position = 'right') +
+  labs(x = 'Time since application (h)', y = expression(Log[10]~'flux'~(kg~h^'-1'~ha^'-1')), colour = 'Parameter set', lty = '')
+ggsave2x('../plots/r5_demo_slow', height = 2.3, width = 3.5)
