591bd7ae274271626f196a8b4d6557933593033c
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-03-24 14:54:46 -0400

    Use ps p1

--- sens/scripts/load_pars03.R
+++ sens/scripts/load_pars03.R
@@ -1,9 +1,6 @@
 
 pars <- fread('../../pars/output/pars.csv')
 
-pars03 <- as.numeric(pars[pars == 'c', ])
+pars03 <- as.numeric(pars[pars == 'p1', ])
 names(pars03) <- colnames(pars)
 pars03 <- pars03[!is.na(pars03)]
-
-# Add incorp pars from pars02
-pars03 <- c(pars03, ALFAM2pars02[grepl('incorp', names(ALFAM2pars02))])
