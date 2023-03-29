d8b2c49b8fbef326a3f28bcf9be9252a454a656a
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-03-29 14:28:20 -0400

    sel plot

--- pars/scripts/plot_pars.R
+++ pars/scripts/plot_pars.R
@@ -12,3 +12,14 @@ ggplot(parsl, aes(reorder(parameter, ppar), value, colour = pars, group = pars))
   labs(x = 'Secondary parameter', y = 'Value', colour = 'Par. set') +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 ggsave2x('../plots-pars/pars', height = 4.4, width = 6)
+
+dd <- subset(parsl, pars %in% c('ps1', 'ps2', 'p1'))
+dd[, pars.nm := factor(pars, levels = c('ps1', 'ps2', 'p1'), labels = 1:3)]
+ggplot(dd, aes(reorder(parameter, ppar), value, colour = pars.nm, group = pars)) +
+  geom_point(size = 2, shape = 1) +
+  geom_line() +
+  labs(x = 'Secondary parameter', y = 'Value', colour = 'Parameter\nset') +
+  scale_colour_viridis_d() +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
+ggsave2x('../plots-pars/pars_sel', height = 3.4, width = 5)
