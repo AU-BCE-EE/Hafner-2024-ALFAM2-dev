6b621216e213e9034b8cb5a3ccd52e92c331e4b1
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-05-24 09:43:52 -0400

    Work on ignoring database csv files

--- data-emission/scripts/main.R
+++ data-emission/scripts/main.R
@@ -4,7 +4,7 @@ rm(list = ls())
 
 # Set release tag for download
 ghpath <- 'https://github.com/sashahafner/ALFAM2-data/raw/'
-rtag <- 'v2.19'
+rtag <- 'v2.21'
 
 ## Alternative below to get particular commit (if not yet in release)
 #ghpath <- 'https://github.com/sashahafner/ALFAM2-data/raw/9291c6fff58463e042e943559c129cb6d5a1248e/'
