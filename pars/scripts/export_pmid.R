c92a6f8a7118187eaf8d9eeeb67ff56b23d44d29
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-06-01 06:03:16 -0400

    Export pmid

---
+++ pars/scripts/export_pmid.R
@@ -0,0 +1,12 @@
+# Save pmid for each subset (after adding in plots in prep.R) for reproducibility
+
+pmid1 <- sort(pdat1[, pmid])
+write.table(pmid1, '../output/pmid_sub1.txt', row.names = FALSE, col.names = FALSE)
+
+pmid2 <- sort(pdat2[, pmid])
+write.table(pmid2, '../output/pmid_sub2.txt', row.names = FALSE, col.names = FALSE)
+
+pmid3 <- sort(pdat3[, pmid])
+write.table(pmid3, '../output/pmid_sub3.txt', row.names = FALSE, col.names = FALSE)
+
+
