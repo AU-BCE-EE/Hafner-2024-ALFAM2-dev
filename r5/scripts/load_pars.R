2663b32c97f49a682ff51d6df9641264d44ad4bd
Sasha D. Hafner <sasha.hafner@gmail.com>
2023-09-08 13:19:06 +0200

    Remove ps03 from plots, add combined plot

--- r5/scripts/load_pars.R
+++ r5/scripts/load_pars.R
@@ -1,8 +1,3 @@
 
-pars <- fread('../../pars/output/pars.csv')
-pars03 <- as.numeric(pars[pars == 'c', ])
-names(pars03) <- colnames(pars)
-pars03 <- pars03[!is.na(pars03)]
-
 pars025 <- ALFAM2pars02
-pars025 <- c(pars025, int.r5 = -2.1)
+pars025 <- c(pars025, int.r5 = -1.8, rain.rate.r5 = 0.7)
--- r5/scripts/plot.R
+++ r5/scripts/plot.R
@@ -20,3 +20,17 @@ ggplot(preds, aes(ct, s, colour = pars, lty = rain, group = sim)) +
   theme(legend.position = 'right') +
   labs(x = 'Time since application (h)', y = expression(Log[10]~'flux'~(kg~h^'-1'~ha^'-1')), colour = 'Parameter set', lty = '')
 ggsave2x('../plots/r5_demo_slow', height = 2.3, width = 3.5)
+
+# Make long
+preds[, lj := log10(j)]
+pl <- melt(preds, id.vars = c('ct', 'pars', 'rain', 'sim'), measure.vars = c('lj', 'er')) 
+pl[, var := factor(variable, levels = c('lj', 'er'), labels = c('Log flux', 'Relative emission'))]
+
+ggplot(pl, aes(ct, value, colour = pars, lty = rain, group = sim)) +
+  geom_line(alpha = 0.7) +
+  theme_bw() +
+  theme(legend.position = 'right') +
+  facet_wrap(~var, scale = 'free') +
+  labs(x = 'Time since application (h)', y = 'Flux or cum. emis.', colour = 'Parameter set', lty = '') +
+  theme(axis.text.y = element_blank())
+ggsave2x('../plots/r5_demo_both', height = 2.3, width = 6)
