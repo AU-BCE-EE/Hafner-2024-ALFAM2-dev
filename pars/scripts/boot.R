1a777df1b0741b5463b977f83265397a47bc4bb7
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-10-24 13:41:25 -0400

    Run quick boot with few optim iterations

--- pars/scripts/boot.R
+++ pars/scripts/boot.R
@@ -1,8 +1,8 @@
 # Bootstrap pars
 # See crossval.R for notes
 
-pars.cal <- mods$ps3$cal$par * 0.8
-fixed <- numeric()
+pars.start <- mods$ps3$optim$par
+fixed <- c(int.r5 = -1.8)
 
 nb <- 100
 inst.all <- unique(idat1[, inst])
@@ -33,11 +33,11 @@ for (i in 1:nb) {
   # Calibration
   mods.boot[[i]] <- list()
   mods.boot[[i]][['inst']] <- inst.samp
-  mods.boot[[i]][['cal']] <- m <- optim(par = pars.cal, fn = function(par) 
-                                    resCalcComb(p = par, dat = idatsamp, to = c('er', 'j'), wr = 4 / 1, time.name = 'cta',
-                                            app.name = 'tan.app', group = 'pmid', fixed = fixed, method = 'SS', 
-                                            weights = idatsamp[, .(weight.last, weight.1a)], flatout = TRUE),
-                                    method = 'Nelder-Mead', control = list(maxit = maxit2))
+  mods.boot[[i]][['optim']] <- m <- optim(par = pars.start, fn = function(par) 
+                                          resCalcComb(p = par, dat = idatsamp, to = c('er', 'j'), wr = 4 / 1, time.name = 'cta',
+                                                  app.name = 'tan.app', group = 'pmid', fixed = fixed, method = 'SS', 
+                                                  weights = idatsamp[, .(weight.lastc, weight.1c)], flatout = TRUE),
+                                          method = 'Nelder-Mead', control = list(maxit = maxit1))
 
   mods.boot[[i]][['coef']] <- c(m$par, fixed)
   
