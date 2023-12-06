db4d623d43baa77b9e789085e3e02410c502f373
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-12-06 10:58:39 -0500

    Plots to look at long emis.
    r5 question, some level off, some do not

---
+++ long/scripts/load.R
@@ -0,0 +1,10 @@
+# Load ALFAM2 data
+
+pdat <- fread('../../data-emission/data/ALFAM2_plot.csv')
+
+# Couple minor changes
+pdat[, app.mthd := app.method]
+pdat[, inst := factor(inst)]
+
+# Get pmid from par est subset 1
+ps3pmid <- scan('../../pars/output/pmid_sub1.txt')
---
+++ long/scripts/main.R
@@ -0,0 +1,8 @@
+
+rm(list = ls())
+
+source('packages.R')
+source('functions.R')
+source('load.R')
+source('process.R')
+source('plot.R')
