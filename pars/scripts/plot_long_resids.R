66b6d328109fd293fb8c6e77e17c003e4069a67b
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-12-12 14:46:12 -0500

    Add long resid plots

---
+++ pars/scripts/plot_long_resids.R
@@ -0,0 +1,36 @@
+ls()
+
+dpreds[, pars.nm := factor(pars, levels = c('ps1', 'ps2', 'ps3'), labels = 1:3)]
+
+dd <- subset(dpreds, pars %in%  c('ps1', 'ps2', 'ps3') & dataset == 1)
+ggplot(dd, aes(cta, resid.er, colour = factor(country), group = pmid)) +
+  geom_line(alpha = 0.6) +
+  facet_grid(app.mthd.nm ~ pars.nm) +
+  coord_cartesian(xlim = c(0, 500)) +
+  labs(x = 'Time (h)', y = 'Model error (frac. applied TAN)', colour = 'Country') +
+  theme(legend.position = 'top') +
+  theme_bw() +
+ggsave2x('../plots-resids/long_resids', height = 6, width = 7)
+
+dd <- subset(dpreds, pars %in%  c('ps1', 'ps2', 'ps3') & dataset == 1 & cta <= 500)
+dw <- dcast(dd, country + app.mthd.nm + pmid + cta ~ pars, value.var = 'resid.er')
+ggplot(dw, aes(ps2, ps3, colour = cta, group = pmid)) +
+  geom_abline(intercept = 0, slope = 1) +
+  geom_point() +
+  geom_line(alpha = 0.3) +
+  facet_wrap(~ app.mthd.nm) +
+#   coord_cartesian(xlim = c(0, 500)) +
+  scale_color_gradientn(colours = rainbow(5)) +
+  labs(x = 'Time (h)', y = 'Model error (frac. applied TAN)', colour = 'Country') +
+  theme(legend.position = 'top')
+ggsave2x('../plots-resids/long_resids_scatter', height = 6, width = 7)
+
+dd <- subset(dpreds, pars %in%  c('ps1', 'ps2', 'ps3') & dataset == 1 & cta > 48 & !is.na(resid.er) & !(app.mthd == 'cs' & pars == 'ps1'))
+dls <- dd[, as.list(coef(lm(resid.er ~ cta))), by = .(pmid, app.mthd, app.mthd.nm, pars,  pars.nm)]
+ggplot(dls, aes(app.mthd.nm, cta, fill = pars.nm)) +
+  geom_boxplot() +
+  theme_bw() +
+  theme(legend.position = 'top') +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(x = 'Application method', y = expression(1000 %*% 'slope in residuals'), fill = 'Parameter set') 
+ggsave2x('../plots-resids/long_resids_box', height = 4, width = 4)
