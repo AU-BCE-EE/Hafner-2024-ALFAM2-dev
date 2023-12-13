759d8c2bf020759712190c587464a0831cad5199
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-12-13 06:10:39 -0500

    Tweaks to pars plot

--- pars/scripts/labels.R
+++ pars/scripts/labels.R
@@ -5,6 +5,38 @@ parsl[, ppar := gsub('^.+([fr][0-5])$', '\\1', parameter)]
 spars <- unique(parsl[, parameter])
 parsl[, parameter := factor(parameter, levels = spars[order(gsub('^.+[fr]([0-5])$', '\\1', spars))])]
 parsl[, parameter := gsub('\\.ni', '', parameter)]
+parsl[, parameter := factor(parameter, levels = c('int.f0', 
+                                                  'int.r1', 
+                                                  'int.r2', 
+                                                  'int.r3', 
+                                                  'int.r5', 
+                                                  'app.mthd.bc.r1', 
+                                                  'app.mthd.ts.r1', 
+                                                  'app.mthd.os.f0', 
+                                                  'app.mthd.cs.f0', 
+                                                  'app.rate.f0', 
+                                                  'man.dm.f0', 
+                                                  'man.dm.r1', 
+                                                  'man.source.pig.f0',
+                                                  'air.temp.r1', 
+                                                  'wind.2m.r1', 
+                                                  'man.ph.r1', 
+                                                  'rain.rate.r2', 
+                                                  'app.mthd.bc.r3', 
+                                                  'app.mthd.os.r3', 
+                                                  'app.mthd.cs.r3', 
+                                                  'man.ph.r3', 
+                                                  'incorp.shallow.f4', 
+                                                  'incorp.shallow.r3', 
+                                                  'incorp.deep.f4', 
+                                                  'incorp.deep.r3', 
+                                                  'rain.rate.r5', 
+                                                  'wind.sqrt.r1', 
+                                                  'man.dm.sqrt.f0', 
+                                                  'air.temp.r3', 
+                                                  'rain.cum.r3', 
+                                                  'ts.cereal.hght.r1'))]
+parsl[, pars.nm := factor(pars, levels = c('ps1', 'ps2', 'ps3'), labels = 1:3)]
 
 # Add some descriptive variables for plots
 dp168[, `:=` (acid.nm = factor(ifelse(acid, 'Acidified', 'Reference'), levels = c('Reference', 'Acidified')), 
--- pars/scripts/plot_pars.R
+++ pars/scripts/plot_pars.R
@@ -1,6 +1,6 @@
 # Plot parameter values
 
-ggplot(parsl, aes(reorder(parameter, ppar), value, colour = pars, group = pars)) +
+ggplot(parsl, aes(reorder(parameter, ppar), value, colour = pars.nm, group = pars.nm)) +
   geom_hline(yintercept = 0, lty = 2) +
   geom_point(size = 2, shape = 1, alpha = 0.5) +
   geom_line() +
@@ -10,13 +10,14 @@ ggplot(parsl, aes(reorder(parameter, ppar), value, colour = pars, group = pars))
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
 ggsave2x('../plots-pars/pars_all', height = 5, width = 9)
 
-dd <- subset(parsl, pars %in% c('ps1', 'ps2', 'ps3'))
-ggplot(dd, aes(reorder(parameter, ppar), value, colour = pars, group = pars)) +
+# Keep NA values to get breaks in lines
+dd <- droplevels(subset(parsl, pars %in% c('ps1', 'ps2', 'ps3') & !is.na(value)))
+ggplot(dd, aes(reorder(parameter, ppar), value, colour = pars.nm, group = pars.nm)) +
   geom_hline(yintercept = 0, lty = 2) +
-  geom_point(size = 2, shape = 1, alpha = 0.5) +
-  geom_line() +
+  geom_point(size = 2, alpha = 0.8) +
+  geom_line(alpha = 0.5) +
   facet_wrap(~ ppar, scale = 'free') +
   labs(x = 'Secondary parameter', y = 'Parameter value', colour = 'Parameter\nset') +
   theme_bw() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) 
-ggsave2x('../plots-pars/pars_sel', height = 5, width = 9)
+ggsave2x('../plots-pars/pars_sel', height = 5, width = 6)
