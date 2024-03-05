c98d93069875af807feb33c7cede0d3d4ff9f5d8
Sasha Hafner <35272876+sashahafner@users.noreply.github.com>
2024-03-05 09:09:51 -0500

    Update main.R - remove old hash url

--- data-emission/scripts/main.R
+++ data-emission/scripts/main.R
@@ -7,7 +7,6 @@ rm(list = ls())
 #rtag <- 'v2.33'
 
 ## Alternative below to get particular commit (if not yet in release)
-#ghpath <- 'https://github.com/sashahafner/ALFAM2-data/raw/9291c6fff58463e042e943559c129cb6d5a1248e/'
 ghpath <- 'https://github.com/sashahafner/ALFAM2-data/raw/fcf102525c88d5eb325d1b81348a5595a00f53a0/'
 rtag <- ''
 
