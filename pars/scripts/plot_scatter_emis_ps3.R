66ce8305b1c5595c823f0ca52f5b855dda6acbd7
Sasha D. Hafner <sasha@hafnerconsulting.com>
2024-04-18 10:20:22 -0400

    tweak plot

--- pars/scripts/plot_scatter_emis_ps3.R
+++ pars/scripts/plot_scatter_emis_ps3.R
@@ -6,21 +6,20 @@ ggplot(ddf, aes(er.pred, er, colour = inst, shape = set)) +
   geom_abline(intercept = 0, slope = 1, col = 'gray45') +
   geom_point(bg = 'gray45', size = 3, alpha = 0.8) +
   scale_shape_manual(values = c(20, 2, 3, 1)) +
-  facet_wrap( ~ app.mthd.nm, ncol = 2) +
-  scale_color_viridis_d() +
+  facet_wrap( ~ app.mthd.nm, ncol = 3) +
+  #scale_color_viridis_d() +
   labs(x = 'ALFAM2 par. set 3 calculated emission (frac. applied TAN)', 
        y = 'Measured emission (frac. applied TAN)',
        colour = '', shape = '') +
   theme_bw() +
-  theme(legend.position = 'right') +
+  theme(legend.position = c(0.8, 0.27)) +
   guides(color = 'none') +
   xlim(0, 1) +
   ylim(0, 1) +
-  guides(colour = guide_legend(ncol = 1)) +
   coord_fixed()
-ggsave2x('../plots-scatter/e168_comp_ps3_paper', height = 7, width = 6)
+ggsave2x('../plots-scatter/e168_comp_ps3_paper', height = 5.5, width = 6)
 
-# Repeat, but highlight institutionso
+# Repeat, but highlight institutions
 for (i in unique(ddf[, inst])) {
 
   dd <- ddf[inst == i, ]
